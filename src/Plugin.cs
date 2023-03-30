f496db3ba6ecf7d08718b2d9a3c060e32785dbbc
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-03-30 00:14:47 -0400

    Update mod

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using System.Linq;
 using System.Security.Permissions;
 
 // Allows access to private members
@@ -6,28 +7,58 @@ using System.Security.Permissions;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
+namespace ScavBarrel;
 
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+using static CreatureTemplate.Type;
+
+[BepInPlugin("com.dual.barrel-of-scavs", "Barrel of Scavs", "1.0.0")]
 sealed class Plugin : BaseUnityPlugin
 {
-    bool init;
+    static bool CanDragIntoDen(AbstractCreature self, AbstractCreature other)
+    {
+        var ty = self.creatureTemplate.TopAncestor().type;
+        if (ty == Fly || ty == Centipede || ty == EggBug || ty == Leech) {
+            return false;
+        }
+        if (ty == PoleMimic || ty == TentaclePlant || ty == MirosBird || ty == Vulture) {
+            return true;
+        }
+        return self.creatureTemplate.CreatureRelationship(other.creatureTemplate).type == CreatureTemplate.Relationship.Type.Eats;
+    }
 
     public void OnEnable()
     {
-        // Add hooks here
-        On.RainWorld.OnModsInit += OnModsInit;
+        On.AbstractWorldEntity.IsEnteringDen += AbstractWorldEntity_IsEnteringDen;
+        On.Creature.FlyAwayFromRoom += Creature_FlyAwayFromRoom;
     }
 
-    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    private void AbstractWorldEntity_IsEnteringDen(On.AbstractWorldEntity.orig_IsEnteringDen orig, AbstractWorldEntity self, WorldCoordinate den)
     {
-        orig(self);
+        orig(self, den);
 
-        if (init) return;
+        if (self is AbstractPhysicalObject apo && apo.stuckObjects.Any(s => s is AbstractPhysicalObject.CreatureGripStick c && c.B == apo)) {
+            if (apo.realizedObject is Creature c) {
+                c.LoseAllGrasps();
+            }
+            apo.LoseAllStuckObjects();
+        }
+        else if (self is AbstractCreature crit) {
+            foreach (var stick in crit.stuckObjects.ToList().OfType<AbstractPhysicalObject.CreatureGripStick>()) {
+                if (stick.B is AbstractCreature prey && !CanDragIntoDen(crit, prey)) {
+                    stick.Deactivate();
+                }
+            }
+        }
+    }
 
-        init = true;
-
-        // Initialize assets, your mod config, and anything that uses RainWorld here
-        Logger.LogDebug("Hello world!");
+    private void Creature_FlyAwayFromRoom(On.Creature.orig_FlyAwayFromRoom orig, Creature self, bool carriedByOther)
+    {
+        if (carriedByOther) {
+            self.LoseAllGrasps();
+            self.abstractCreature?.LoseAllStuckObjects();
+        }
+        else {
+            orig(self, carriedByOther);
+        }
     }
 }
