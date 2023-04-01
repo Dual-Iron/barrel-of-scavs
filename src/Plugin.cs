dd986356053d44083d60c9da1da45b2693fbd698
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-03-31 22:39:15 -0400

    Finish mod

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
+using RWCustom;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Permissions;
+using UnityEngine;
 
 // Allows access to private members
 #pragma warning disable CS0618
@@ -9,13 +13,24 @@ using System.Security.Permissions;
 
 namespace ScavBarrel;
 
+using static AbstractPhysicalObject;
 using static CreatureTemplate.Type;
 
-[BepInPlugin("com.dual.barrel-of-scavs", "Barrel of Scavs", "1.0.0")]
+[BepInPlugin("com.dual.barrel-of-scavs", "Barrel of Scavs", "1.0.1")]
 sealed class Plugin : BaseUnityPlugin
 {
-    static bool CanDragIntoDen(AbstractCreature self, AbstractCreature other)
+    sealed class ObjData { public WorldCoordinate startPos; }
+
+    static readonly ConditionalWeakTable<AbstractPhysicalObject, ObjData> objData = new();
+
+    static ObjData Data(AbstractPhysicalObject d) => objData.GetValue(d, _ => new());
+
+    static bool CanDragIntoShortcut(AbstractCreature self, AbstractCreature other)
     {
+        if (self.state.dead || self.realizedCreature is Creature c && !c.Consious) {
+            return false;
+        }
+
         var ty = self.creatureTemplate.TopAncestor().type;
         if (ty == Fly || ty == Centipede || ty == EggBug || ty == Leech) {
             return false;
@@ -26,39 +41,136 @@ sealed class Plugin : BaseUnityPlugin
         return self.creatureTemplate.CreatureRelationship(other.creatureTemplate).type == CreatureTemplate.Relationship.Type.Eats;
     }
 
-    public void OnEnable()
+    static void DropEverything(AbstractCreature crit)
     {
-        On.AbstractWorldEntity.IsEnteringDen += AbstractWorldEntity_IsEnteringDen;
-        On.Creature.FlyAwayFromRoom += Creature_FlyAwayFromRoom;
+        List<AbstractPhysicalObject> release = new();
+
+        for (int i = crit.stuckObjects.Count - 1; i >= 0; i--) {
+            AbstractObjectStick stick = crit.stuckObjects[i];
+
+            if (stick is CreatureGripStick cgs && cgs.A == crit && cgs.B != crit) {
+                cgs.Deactivate();
+                release.Add(cgs.B);
+                if (crit.realizedObject is Creature cr && cr.grasps?[cgs.grasp] != null) {
+                    cr.ReleaseGrasp(cgs.grasp);
+                }
+            }
+            else if (stick is not CreatureGripStick) {
+                stick.Deactivate();
+                if (stick.A != crit)
+                    release.Add(stick.A);
+                if (stick.B != crit)
+                    release.Add(stick.B);
+            }
+        }
+
+        foreach (var item in release) {
+            ResetPos(item);
+        }
     }
 
-    private void AbstractWorldEntity_IsEnteringDen(On.AbstractWorldEntity.orig_IsEnteringDen orig, AbstractWorldEntity self, WorldCoordinate den)
+    static void ResetPos(AbstractPhysicalObject spit)
     {
-        orig(self, den);
+        foreach (var item in spit.GetAllConnectedObjects()) {
+            if (item.realizedObject is not PhysicalObject o) continue;
 
-        if (self is AbstractPhysicalObject apo && apo.stuckObjects.Any(s => s is AbstractPhysicalObject.CreatureGripStick c && c.B == apo)) {
-            if (apo.realizedObject is Creature c) {
-                c.LoseAllGrasps();
+            WorldCoordinate startPos = Data(spit).startPos;
+            Room newRoom = spit.world.GetAbstractRoom(startPos).realizedRoom;
+            if (newRoom == null) {
+                continue;
             }
-            apo.LoseAllStuckObjects();
+
+            startPos.y = Mathf.Clamp(startPos.y, -1, newRoom.TileHeight);
+
+            spit.pos = startPos;
+
+            Room oldRoom = o.room;
+            if (!newRoom.updateList.Contains(o)) {
+                newRoom.AddObject(o);
+            }
+            if (o is Creature c) {
+                c.SpitOutOfShortCut(startPos.Tile, newRoom, false);
+            }
+            if (newRoom != oldRoom) {
+                o.NewRoom(newRoom);
+            }
+
+            if (o is Weapon w) {
+                w.ChangeMode(Weapon.Mode.Free);
+            }
+
+            foreach (BodyChunk chunk in o.bodyChunks) {
+                chunk.HardSetPosition(newRoom.MiddleOfTile(startPos.Tile) + Custom.RNV());
+                chunk.vel = new(0, 1);
+            }
         }
-        else if (self is AbstractCreature crit) {
-            foreach (var stick in crit.stuckObjects.ToList().OfType<AbstractPhysicalObject.CreatureGripStick>()) {
-                if (stick.B is AbstractCreature prey && !CanDragIntoDen(crit, prey)) {
+    }
+
+    public void OnEnable()
+    {
+        // TODO test miros
+
+        On.PhysicalObject.Update += PhysicalObject_Update;
+        On.Creature.SuckedIntoShortCut += PreventDragging;
+        On.AbstractPhysicalObject.IsEnteringDen += DropInDen;
+        On.Creature.FlyAwayFromRoom += DropInSky;
+    }
+
+    private void PhysicalObject_Update(On.PhysicalObject.orig_Update orig, PhysicalObject self, bool eu)
+    {
+        if (self is not Creature c || !c.inShortcut) {
+            Data(self.abstractPhysicalObject).startPos = self.abstractPhysicalObject.pos;
+        }
+
+        orig(self, eu);
+    }
+
+    private void PreventDragging(On.Creature.orig_SuckedIntoShortCut orig, Creature self, IntVector2 entrancePos, bool carriedByOther)
+    {
+        if (self.room?.GetTile(entrancePos)?.Terrain == Room.Tile.TerrainType.ShortcutEntrance) {
+            AbstractCreature grabber = self.abstractCreature;
+
+            foreach (var stick in grabber.stuckObjects.ToList().OfType<CreatureGripStick>()) {
+                if (stick.A == grabber && stick.B is AbstractCreature grabbed && !CanDragIntoShortcut(grabber, grabbed)) {
                     stick.Deactivate();
+                    if (grabber.realizedObject is Creature cr && cr.grasps?[stick.grasp] != null) {
+                        cr.ReleaseGrasp(stick.grasp);
+                    }
+                    Logger.LogDebug($"Preventing {grabber} dragging {grabbed} into shortuct");
                 }
             }
         }
+
+        orig(self, entrancePos, carriedByOther);
     }
 
-    private void Creature_FlyAwayFromRoom(On.Creature.orig_FlyAwayFromRoom orig, Creature self, bool carriedByOther)
+    private void DropInDen(On.AbstractPhysicalObject.orig_IsEnteringDen orig, AbstractPhysicalObject self, WorldCoordinate den)
     {
-        if (carriedByOther) {
-            self.LoseAllGrasps();
-            self.abstractCreature?.LoseAllStuckObjects();
+        if (self is not AbstractCreature predator) {
+            return;
         }
-        else {
-            orig(self, carriedByOther);
+
+        foreach (var stick in predator.stuckObjects) {
+            if (stick is CreatureGripStick cgs && cgs.A == predator && cgs.B is AbstractCreature grabbed && grabbed.stuckObjects.Count > 1) {
+                Logger.LogDebug($"Made {grabbed} drop everything while dragged into den");
+                DropEverything(grabbed);
+            }
         }
+
+        orig(self, den);
+    }
+
+    private void DropInSky(On.Creature.orig_FlyAwayFromRoom orig, Creature self, bool carriedByOther)
+    {
+        if (!carriedByOther && self is Vulture) {
+            foreach (var item in self.grasps) {
+                if (item?.grabbed is Creature c) {
+                    Logger.LogDebug($"Made {c.abstractCreature} drop everything while dragged offscreen");
+                    DropEverything(c.abstractCreature);
+                }
+            }
+        }
+
+        orig(self, carriedByOther);
     }
 }
